255d75a54cd474b78e854d914c565d060895f3ec
Mayuri Kawale <mayurikawale26@gmail.com>
2024-11-06 22:11:01 -0600

    added more visualization code, kNN code for missing values

--- Rscripts/ExploratoryDataAnalysis.R
+++ Rscripts/ExploratoryDataAnalysis.R
@@ -8,14 +8,22 @@ library(stats)
 library(utils)
 
 library(ggplot2)     # for ggplot
+library(gridExtra)   # for grid.arrange
+library(ggcorrplot)  # for ggcorplot
+library(reshape2)    # for melted correlation matrix
 library(tidyverse)
 library(dplyr)
 library(forcats)     # for fct_lump
+library(VIM)         # for kNN
+library(fastDummies) # for dummy_cols
 
 ############################## Dataset ############################################
 
 ## load the dataset
 creditRisk <- read.csv("dataset/credit_risk_dataset.csv")
+
+creditRisk <- creditRisk %>%
+  dplyr::mutate(across(where(is.character), as.factor))
 
 ##################### Exploratory Data Analysis/Visualizations ###################
 
@@ -34,6 +42,7 @@ ggplot(data = creditRisk, aes(x=person_home_ownership, y=loan_percent_income, co
         legend.key.size = unit(0.5, "cm"))       # Reduce the size of legend keys)
 dev.off() 
 
+#---------------------------------------------------------------------------------#
 
 #Histogram for checking skewness: person_income
 png("images/annualIncome.png", width = 700, height = 500)
@@ -46,6 +55,7 @@ ggplot(data=creditRisk, aes(x=person_income))+
   theme(plot.title = element_text(hjust = 0.5))
 dev.off()  
 
+#---------------------------------------------------------------------------------#
 
 # Pie chart for loan intent distribution 
 loan_intent_data <- creditRisk %>%
@@ -63,19 +73,136 @@ ggplot(loan_intent_data, aes(x = "", y = pct, fill = loan_intent)) +
         legend.title = element_text())
 dev.off()
 
+#---------------------------------------------------------------------------------#
+
+## Bar plot for loan approval vs loan intent
+
+# Loan Approval Rate by Loan Intent
+loan_intent_approval <- creditRisk %>%
+  group_by(loan_intent) %>%
+  summarize(approval_rate = mean(loan_status == 1, na.rm = TRUE)) %>%
+  arrange(desc(approval_rate))
+
+# Print the calculated approval rates
+print(loan_intent_approval)
+
+#plot
+png("images/LoanApproval_Intent.png", width = 700, height = 500)
+ggplot(loan_intent_approval, aes(x = loan_intent, y = approval_rate, fill = loan_intent)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label = scales::percent(approval_rate, accuracy = 0.1)), 
+            vjust = -0.5, size = 3) +
+  labs(title = "Loan Approval Rate by Loan Intent", 
+       x = "Purpose of loan", 
+       y = "Loan Approval Rate") +
+  theme_bw() +
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = 0.5))
+dev.off()
+
+#---------------------------------------------------------------------------------#
+
+# homeownership vs approval rate
+plot1 <- ggplot(creditRisk, aes(x = person_home_ownership, fill = as.factor(loan_status))) +
+  geom_bar(position = "fill") + 
+  labs(x = "Home Ownership type", y = "Proportion of loan approval", 
+       title = "Relationship between Home Ownership and Loan Status", 
+       fill = "Loan status") +
+  theme_bw()+
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.title = element_text(size = 10),  # Adjust legend title size
+        legend.text = element_text(size = 8),    # Adjust legend item text size
+        legend.key.size = unit(0.5, "cm"))       # Reduce the size of legend keys
 
 
+#loan intent vs approval rate
+plot2 <- ggplot(creditRisk, aes(x = loan_intent, fill = as.factor(loan_status))) +
+  geom_bar(position = "fill") + 
+  labs(x = "Purpose of loan", y = "Proportion", 
+       title = "Relationship between Loan Intent and Loan Status",
+       fill = "Loan status") +
+  theme_bw()+
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x = element_text(angle = 10, size=6, vjust = 0.6, hjust = 0.5),   # angle rotates the tick labels; vjust and hjust control the vertical and horizontal justification
+        legend.title = element_text(size = 10),                           # Adjust legend title size
+        legend.text = element_text(size = 8),                             # Adjust legend item text size
+        legend.key.size = unit(0.5, "cm"))                                # Reduce the size of legend keys
+  
+
+#loan grade vs approval rate
+plot3 <- ggplot(creditRisk, aes(x = loan_grade, fill = as.factor(loan_status))) +
+  geom_bar(position = "fill") + 
+  labs(x = "Loan Grade", y = "Proportion", 
+       title = "Relationship between Loan Grade and Loan Status",
+       fill="Loan status") +
+  theme_bw()+
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.title = element_text(size = 10),                           # Adjust legend title size
+        legend.text = element_text(size = 8),                             # Adjust legend item text size
+        legend.key.size = unit(0.5, "cm"))
+
+#default history vs approval rate
+
+plot4 <- ggplot(creditRisk, aes(x = cb_person_default_on_file, fill = as.factor(loan_status))) +
+  geom_bar(position = "fill") + 
+  labs(x = "Default History", y = "Proportion", 
+       title = "Relationship between Default history and Loan Status",
+       fill="Loan status") +
+  theme_bw()+
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.title = element_text(size = 10),                           # Adjust legend title size
+        legend.text = element_text(size = 8),                             # Adjust legend item text size
+        legend.key.size = unit(0.5, "cm"))
+
+png("images/loanApprovalRelations.png", width = 800, height = 800)
+grid.arrange(plot1, plot2, plot3, plot4, ncol = 2)
+dev.off()
+
+#---------------------------------------------------------------------------------#
+
+# Heatmap/ correlation map
+corr_data <- dummy_cols(creditRisk, 
+             select_columns = c("person_home_ownership", 
+                                "loan_intent", "loan_grade", 
+                                "cb_person_default_on_file"),
+             remove_first_dummy = FALSE, 
+             remove_selected_columns = TRUE)
 
 
+corr_data[] <- lapply(corr_data, function(x) as.numeric(as.character(x)))
+cor_matrix <- cor(corr_data, use = "complete.obs")
 
+# Filter correlation matrix, retain only values where |correlation| > 0.5
+cor_matrix_filtered <- ifelse(abs(cor_matrix) > 0.5, cor_matrix, 0)
+
+# Convert matrix to long format for plotting
+melted_cor_matrix <- melt(cor_matrix_filtered)
+
+png("images/corrMatrix.png", width = 1000, height = 800)
+ggplot(data = melted_cor_matrix, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile(color = "black") +
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", midpoint = 0,
+                       limit = c(-1, 1), name = "Correlation") +
+  theme_minimal(base_size = 15) +
+  labs(title = "Heatmap of Filtered Correlation Matrix (|correlation| > 0.5)",
+       x = "", y="") +
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x = element_text(angle = 45, hjust = 1))
+dev.off()
+
+  
 ######################## Handling Missing Values #########################################
 
 ## Only two variables have missing values: `person_emp_length` and `loan_int_rate`
 
-creditRisk$person_emp_length[is.na(creditRisk$person_emp_length)] <- median(creditRisk$person_emp_length, na.rm=TRUE)
-creditRisk$loan_int_rate[is.na(creditRisk$loan_int_rate)] <- median(creditRisk$loan_int_rate, na.rm=TRUE)
+#creditRisk$person_emp_length[is.na(creditRisk$person_emp_length)] <- median(creditRisk$person_emp_length, na.rm=TRUE)
+#creditRisk$loan_int_rate[is.na(creditRisk$loan_int_rate)] <- median(creditRisk$loan_int_rate, na.rm=TRUE)
 
-#use predictive mean matching or kNN for handling missing values
+# perform kNN imputation kNN using k=5 i.e 5 neighbors
+creditRisk <- kNN(creditRisk, variable = c("person_emp_length", "loan_int_rate"), k=5, 
+                  imp_var = FALSE   # no additional indicator column is created
+                  )
+
 
 
 ######################### Outliers ###############################################
